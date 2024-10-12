d3c4ae41e14345d91df70c86c6629eb3b625702c
Hao Li <1746200599@qq.com>
2024-10-12 08:48:00 +0800

    Update

---
+++ attention.py
@@ -0,0 +1,41 @@
+import torch
+from torch import nn
+import math
+
+x=torch.rand(128,32,512)  # 例子
+d_model=512
+n_head=8
+
+class MutiHeadAttention(nn.Module):
+    def __init__(self,d_model,n_head): # 数据维度 头的数量
+        super(MutiHeadAttention,self).__init__()
+        self.n_head=n_head
+        self.d_model=d_model
+        # Q、K、V
+        self.w_q=nn.Linear(d_model,d_model)   # 全链接层
+        self.w_k=nn.Linear(d_model,d_model)
+        self.w_v=nn.Linear(d_model,d_model)
+        self.w_combine=nn.Linear(d_model,d_model)  # 合并线性层
+        self.softmax=nn.Softmax(dim=1)   # softmax激活，使dim维度上的数值相加等于1，做归一化？
+    # 处理Q、K、V的关系
+    def forward(self,q,k,v,mask=None):  # mask可选
+        batch,time,dimen=q.shape  # q是输入
+        n_d=int(self.d_model/self.n_head)  # 模型的总维度被平均分，每个头处理n_d个
+        q=self.w_q(q)  # qkv重塑为多头形状
+        k=self.w_k(k)
+        v=self.w_k(v)
+
+        q=q.view(batch,time,self.n_head,n_d).permute(0,2,1,3) #q重塑为4个维度，并交换维度次序
+        k=k.view(batch,time,self.n_head,n_d).permute(0,2,1,3)
+        v=v.view(batch,time,self.n_head,n_d).permute(0,2,1,3)
+        score=q@k.transpose(2,3)/math.sqrt(n_d)  # softmax再乘个V就是Attention
+        if mask is not None: #
+            score=score.masked_fill(mask==0,-10000)  # 掩码为0的地方填充为0.0001，确保softmax
+        score=self.softmax(score)@v  # Attention
+        score=score.permute(0,2,1,3).contiguous().view(batch,time,dimen)  # 重塑一下形状
+        out=self.w_combine(score)
+        return out
+
+attention=MutiHeadAttention(d_model,n_head)
+out=attention(x,x,x)
+print(out)
---
+++ transformer.py
@@ -0,0 +1,42 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+import math
+print(torch.cuda.is_available())
+
+# token embedding 输入的词汇表索引->指定维度embedding
+class TokenEmbedding(nn.Embedding):
+    def __init__(self, vocab_size, d_model): # 词汇表大小，维度
+        super(TokenEmbedding,self).__init__(vocab_size,d_model, padding_idx=1) # 索引为1填充，embedding 初始为0
+
+class PositionEmbedding(nn.Module):
+    def __int__(self, d_model, max_len, device): # 模型维度，最大长度，设备
+        super(PositionEmbedding, self).__init__()
+        self.encoding=torch.zeros(max_len,d_model,device=device)  # [max_len, d_model], 0
+        self.encoding.requires_grad=False
+        pos=torch.arange(0,max_len,device=device)  # 一维向量，start, end, step, device
+        pos=pos.float().unsqueeze(dim=1) #(max_len)->(max_len,1)
+        _2i=torch.arange(0,d_model,2,device=device).float()
+        self.encoding[:,0::2]=torch.sin(pos/(10000**(_2i/d_model)))    # 偶数位置
+        self.encoding[:,1::2]=torch.cos(pos/(10000**(_2i/d_model)))    # 奇数位置
+
+    def forword(self, x):
+            batch_size,seq_len=x.size() #  序列长度
+            return self.encoding[:seq_len,:] # 返回前seq_len的encoding
+
+
+class TransformerEmbedding(nn.Module):
+    def __init__(self, vocab_size, d_model, max_len, drop_prob, device):
+        super(TransformerEmbedding,self).__init__()
+        self.tok_emb=TokenEmbedding(vocab_size,d_model)
+        self.pos_emb=PositionEmbedding(d_model,max_len,device)
+        self.drop_out=nn.Dropout(p=drop_prob) #  随机丢弃神经元，防止过拟合
+
+    def forword(self,x):
+        tok_emb=self.tok_emb(x)  # 初始化tok
+        pos_emb=self.pos_emb(x)  # 初始化pos
+        return self.drop_out(tok_emb+pos_emb)
+
+
+
+
